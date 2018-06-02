7891194538707afa14521bb3bf3688366ab68cbf
Deyclan <brandon.piard@etu.univ-lyon1.fr>
2018-06-02 18:36:46 +0200

    initial commit

---
+++ CerisaieTopic/src/main/java/bean/DemandeInscriptionTopic.java
@@ -0,0 +1,18 @@
+package bean;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
+import javax.naming.NamingException;
+
+public class DemandeInscriptionTopic implements MessageListener {
+
+
+
+    public void onMessage(Message message) {
+
+    }
+
+
+}
---
+++ CerisaieTopic/src/main/java/model/TypeEmplacementEntity.java
@@ -0,0 +1,74 @@
+package model;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+@Table(name = "type_emplacement", schema = "cerisaie", catalog = "")
+public class TypeEmplacementEntity {
+    private int codeTypeE;
+    private String libtypepl;
+    private int tariftypepl;
+    private Collection<EmplacementEntity> emplacementsByCodeTypeE;
+
+    @Id
+    @Column(name = "CodeTypeE", nullable = false)
+    public int getCodeTypeE() {
+        return codeTypeE;
+    }
+
+    public void setCodeTypeE(int codeTypeE) {
+        this.codeTypeE = codeTypeE;
+    }
+
+    @Basic
+    @Column(name = "LIBTYPEPL", nullable = false, length = 30)
+    public String getLibtypepl() {
+        return libtypepl;
+    }
+
+    public void setLibtypepl(String libtypepl) {
+        this.libtypepl = libtypepl;
+    }
+
+    @Basic
+    @Column(name = "TARIFTYPEPL", nullable = false)
+    public int getTariftypepl() {
+        return tariftypepl;
+    }
+
+    public void setTariftypepl(int tariftypepl) {
+        this.tariftypepl = tariftypepl;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TypeEmplacementEntity that = (TypeEmplacementEntity) o;
+
+        if (codeTypeE != that.codeTypeE) return false;
+        if (tariftypepl != that.tariftypepl) return false;
+        if (libtypepl != null ? !libtypepl.equals(that.libtypepl) : that.libtypepl != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = codeTypeE;
+        result = 31 * result + (libtypepl != null ? libtypepl.hashCode() : 0);
+        result = 31 * result + tariftypepl;
+        return result;
+    }
+
+    @OneToMany(mappedBy = "typeEmplacementByCodeTypeE")
+    public Collection<EmplacementEntity> getEmplacementsByCodeTypeE() {
+        return emplacementsByCodeTypeE;
+    }
+
+    public void setEmplacementsByCodeTypeE(Collection<EmplacementEntity> emplacementsByCodeTypeE) {
+        this.emplacementsByCodeTypeE = emplacementsByCodeTypeE;
+    }
+}
